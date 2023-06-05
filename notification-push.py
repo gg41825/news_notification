da4cd6ca98112bc6ed77d8d3a40284954afdc1c5
Ginny Lee <gg41825@gmail.com>
2023-06-05 22:42:40 +0200

    integrated with linebot

--- notification-push.py
+++ notification-push.py
@@ -1,23 +1,45 @@
+from flask import Flask
+from waitress import serve
 import requests
 from bs4 import BeautifulSoup
 import configparser
 
-req_url = ""
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
 
+app = Flask(__name__)
+
+@app.route("/pushnews")
 def getNews():
-  loadConfig()
-  resp = requests.get(req_url)
-  soup = BeautifulSoup(resp.text, "html.parser")
-  news_link = soup.find("a", class_="teaser__link").get("href") #只搜尋第一個符合條件的HTML節點, class為teaser__link的, 取href的值
-  paragraph = soup.find("a", class_="teaser__link")
-  news_title = paragraph.select_one(".teaser__headline").text
+    resp = requests.get(req_url)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    news_link = soup.find("a", class_="teaser__link").get("href") #只搜尋第一個符合條件的HTML節點, class為teaser__link的, 取href的值
+    paragraph = soup.find("a", class_="teaser__link")
+    news_title = paragraph.select_one(".teaser__headline").text
+    try:
+        line_bot_api = LineBotApi(access_token)
+        line_bot_api.push_message(user_id, TextSendMessage(text=f'Your daily news: {news_title}\nLink: {req_url}{news_link}'))
+        return 'OK'
+    except:
+        print("error")
+
+@app.route("/", methods=['GET'])
+def home():
+    return 'Welcome to Ginny bot'
 
 
-def loadConfig(): 
-  global req_url
+def loadConfig():
+  global req_url, access_token, secret, user_id, host, port
+  
   config = configparser.ConfigParser()
   config.read('service.conf')
+  host = config['app']['host']
+  port = config['app']['port']
+
   req_url = config['news']['request.url']
+  access_token = config['news']['line.access.token']
+  user_id = config['news']['line.user.id']
 
 if __name__ == '__main__':
-  getNews()
+    loadConfig()
+    serve(app, host=host, port=port)
